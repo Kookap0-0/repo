17ca4a7eba811fce00581f9eebaf6ec6c48fe7d9
rasp <kolyaseredin128@gmail.com>
2025-09-17 17:23:28 +0300

    5

--- get-led/bin-counter.py
+++ get-led/bin-counter.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as g 
 import time as t 
-leds = [24,22,23,27,17,25,12,16]
+leds = [16,12,25,17,27,23,22,24]
 sleep_time = 0.2
 up = 9
 down = 10
@@ -11,14 +11,22 @@ g.setup(up, g.IN)
 g.setup(down, g.IN)
 g.output(leds, 0)
 def f(n):
-    return [int(element) for element in bin(n)][2:].zfill(8)]
+    return [int(element) for element in bin(n)[2:].zfill(8)]
+def r(leds, bin):
+    temp = []
+    for i in range(len(bin)):
+        if bin[i]==1:
+            temp.append(leds[i])
+    return temp
 while(True):
+    g.output(leds, 0) 
     if(g.input(up)):
-        if(num==6):
+        if(num==7):
             num=0
         else:
             num+=1
         print(num, f(num))
+        g.output(r(leds,f(num)), 1)
         t.sleep(sleep_time)
     if(g.input(down)):
         if(num>0):
@@ -26,4 +34,6 @@ while(True):
         else:
             num=0
         print(num, f(num))
+        g.output(r(leds,f(num)), 1)
         t.sleep(sleep_time)
+   
