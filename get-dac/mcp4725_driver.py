1ea8e231f0858cc47fd573c1dab58270c1e93e65
rasp <kolyaseredin128@gmail.com>
2025-10-08 16:41:23 +0300

    dfdfdf

--- get-dac/mcp4725_driver.py
+++ get-dac/mcp4725_driver.py
@@ -17,20 +17,35 @@ class MCP4725:
             print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
             print("Устанавлниваем 0.0 В")
             return
-        duty_cycle = (voltage/self.dynamic_range)*100
-        self.pwm.ChangeDutyCycle(duty_cycle)
-        
+
+    def set_number(self, number):
+        if not isinstance(number, int):
+            print("На вход ЦАП можно подавать только целые числа")
+            return
+        if not (0 <= number <= 4095):
+            print("Число выходит за разраядность MCP4752 (12 бит)")
+            return
+        first_byte = self.wm | self.pds | number >> 8
+        second_byte = number & 0xFF
+        self.bus.write_byte_data(0x61, first_byte, second_byte)
+
+        if self.verbose:
+            print(f"Число: {number}, отправленные по I2C данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")
+
+    def set_voltage(self, voltage):
+        self.set_number(int(4095*voltage/self.dynamic_range))
+
 if __name__ == "__main__":
     try:
-        dac = PWM_DAC(12, 500, 3.290, True)
+        mcp = MCP4725(5.145, 0x61, True)
         
         while True:
             try:
                 voltage = float(input("Введите напряжение в Вольтах: "))
-                dac.set_voltage(voltage)
+                mcp.set_voltage(voltage)
 
             except ValueError:
                 print("Вы ввели не число. Попробуйте ещё раз\n")
 
     finally:
-        dac.deinit()
+        mcp.deinit()
