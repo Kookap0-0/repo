7b17ae08f36e815ee58f49543feb29b395e14a7d
rasp <kolyaseredin128@gmail.com>
2025-10-08 16:19:06 +0300

    dfdf

---
+++ get-dac/mcp4725_driver.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as g
+import smbus
+class MCP4725:
+    def __init__(self, dynamic_range, address = 0x61, verbose = True):
+        self.bus = smbus.SMBus(1)
+        self.address = address
+        self.wm = 0x00
+        self.pds = 0x00
+        self.verbose = verbose
+        self.dynamic_range = dynamic_range
+        
+    def deinit(self):
+        self.bus.close()
+
+    def set_voltage(self,voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
+            print("Устанавлниваем 0.0 В")
+            return
+        duty_cycle = (voltage/self.dynamic_range)*100
+        self.pwm.ChangeDutyCycle(duty_cycle)
+        
+if __name__ == "__main__":
+    try:
+        dac = PWM_DAC(12, 500, 3.290, True)
+        
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
+
+            except ValueError:
+                print("Вы ввели не число. Попробуйте ещё раз\n")
+
+    finally:
+        dac.deinit()
